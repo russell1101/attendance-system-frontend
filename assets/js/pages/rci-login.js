632c8156459c64318d66d26e6a7070ec3a29768e
Marco <marco.pan1998@gmail.com>
2026-03-22 09:47:24 +0800

    feat:

--- assets/js/pages/rci-login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const float_end_btn = document.querySelector("div.float-end");
-    float_end_btn.addEventListener("click", function () {
-        alert("請聯絡後台");
-    })
-});
