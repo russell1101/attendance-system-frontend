518b33792c602fa6c0170decc7e654be21add3bb
Wesley <hwesley1101@gmail.com>
2026-03-30 21:16:24 +0800

    feat: 刪除前台 舊頁面 feat: 完成會員資訊頁功能 feat: 完成商品頁面功能 feat: 完成打卡+歷程功能

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,19 @@
 const APP_CONFIG = {
-    API_BASE_URL: "http://localhost:8080/attendances-project",
+    API_BASE_URL: "http://127.0.0.1:8080/attendances-project",
 
     // 分頁預設筆數
     PAGE_SIZE: 10,
 };
+
+// 全域 SweetAlert2 樣式統一設定
+if (typeof Swal !== 'undefined') {
+    window.Swal = Swal.mixin({
+        buttonsStyling: false, 
+        reverseButtons: true,  
+        customClass: {
+            confirmButton: 'btn btn-primary w-xs ms-2', 
+            cancelButton: 'btn btn-light w-xs',
+            popup: 'rounded-4 shadow-lg'
+        }
+    });
+}
